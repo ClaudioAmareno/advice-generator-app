e4bf5ca3b907fe89b892dd5e899438a6b19e7337
Klaudiusz <klaudecki@gmail.com>
2023-12-03 13:11:05 +0100

    changed js

--- js/main.js
+++ js/main.js
@@ -4,17 +4,12 @@ const btn = document.querySelector('.btn')
 
 const URL = 'https://api.adviceslip.com/advice'
 
-btn.addEventListener('click', () =>{
+btn.addEventListener("click", () => {
     fetch(URL)
-	.then(res => res.json())
-    .then(data => randomAdvice.textContent = ('src', data.slip.advice))
-    .catch(err => console.error(err))
-
-    fetch(URL)
-	.then(res => res.json())
-    .then(data => numberAdvice.textContent = `Advice #${'src', data.slip.id}`)
-    .catch(err => console.error(err))
-
-})
-
-
+        .then(res => res.json())
+        .then(data => {
+            randomAdvice.textContent = ("src", data.slip.advice);
+            numberAdvice.textContent = `Advice #${("src", data.slip.id)}`;
+        })
+        .catch(err => console.error(err));
+});
